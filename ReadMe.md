0d98a69a7aa121152a891eb24a14c65e40ae27bd
W-Lone <aime.saysset@gmail.com>
2023-02-28 10:21:58 +0100

    Readme

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+######################
+Puissance 4
+Développée par Jhon Doe
+Tester par Aimé SAYSSET
+######################
+Tester le 28/02/23
+######################
+Pour la stratégie de test qui a pour but de vous perdre avec des tests d'intégration inexistante 
--- test/unit/test_noyau.py
+++ test/unit/test_noyau.py
@@ -4,3 +4,68 @@ from noyau import *
 
 def test_changer_joueur():
    assert True==True
+
+def test_jeton():
+    grille = [[0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0]]
+    joueur = 1
+    # test avec une seule case occupée par le joueur
+    assert jeton(joueur, 0, 0, 1, 0, grille) == 1
+    assert jeton(joueur, 0, 0, 0, 1, grille) == 1
+    assert jeton(joueur, 0, 0, 1, 1, grille) == 1
+    assert jeton(joueur, 0, 0, 1, -1, grille) == 1
+    #
+
+def test_maj_jeu():
+    grille = [[-1,-1,-1,-1,-1,-1,-1],
+              [-1,-1,-1,-1,-1,-1,-1],
+              [-1,-1,-1,-1,-1,-1,-1],
+              [-1,-1,-1,-1,-1,-1,-1],
+              [-1,-1,-1,-1,-1,-1,-1],
+              [-1,-1,-1,-1,-1,-1,-1],
+              [-1,-1,-1,-1,-1,-1,-1]]
+
+    niveau = [6,6,6,6,6,6,6]
+
+    # joueur 1 joue la case 00
+    joueur = 1
+    case = 'b0'
+    maj_jeu(case, grille, niveau, joueur)
+    assert grille == [[-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [1 ,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1]]
+    assert niveau == [5,6,6,6,6,6,6]
+
+    # joueur 0 joue la case 01
+    joueur = 0
+    case = 'b1'
+    maj_jeu(case, grille, niveau, joueur)
+    assert grille == [[-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [1 ,0 ,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1]]
+    assert niveau == [5,5,6,6,6,6,6]
+
+    # joueur 1 joue la case 11
+    joueur = 1
+    case = 'b1'
+    maj_jeu(case, grille, niveau, joueur)
+    assert grille == [[-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1],
+                      [1 ,0 ,1 ,-1,-1,-1,-1],
+                      [-1,-1,-1,-1,-1,-1,-1]]
+    assert niveau == [5,4,6,6,6,6,6]
