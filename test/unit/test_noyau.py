030502073aa6f33af8873984b7c8aea906681fa3
W-Lone <aime.saysset@gmail.com>
2023-02-28 10:32:19 +0100

    FIN

--- test/unit/test_noyau.py
+++ test/unit/test_noyau.py
@@ -1,71 +1,39 @@
-import pytest
 
-from noyau import *
-
-def test_changer_joueur():
-   assert True==True
-
+# Test unitaire de la fonction jeton
 def test_jeton():
-    grille = [[0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0]]
-    joueur = 1
-    # test avec une seule case occupée par le joueur
-    assert jeton(joueur, 0, 0, 1, 0, grille) == 1
-    assert jeton(joueur, 0, 0, 0, 1, grille) == 1
-    assert jeton(joueur, 0, 0, 1, 1, grille) == 1
-    assert jeton(joueur, 0, 0, 1, -1, grille) == 1
-    #
+    grille = [
+        [1, -1, -1, -1, -1, -1, -1],
+        [0,  1, -1, -1, -1, -1, -1],
+        [0,  0,  1, -1, -1, -1, -1],
+        [0,  0,  0,  1, -1, -1, -1],
+        [0,  0,  0,  0,  0, -1, -1],
+        [0,  0,  0,  0,  0,  0, -1]
+    ]
+    assert jeton(1, 3, 3, -1, -1, grille) == 3
+    assert jeton(1, 2, 2, 1, 1, grille) == 3
+    assert jeton(0, 0, 1, 1, 0, grille) == 0
 
-def test_maj_jeu():
-    grille = [[-1,-1,-1,-1,-1,-1,-1],
-              [-1,-1,-1,-1,-1,-1,-1],
-              [-1,-1,-1,-1,-1,-1,-1],
-              [-1,-1,-1,-1,-1,-1,-1],
-              [-1,-1,-1,-1,-1,-1,-1],
-              [-1,-1,-1,-1,-1,-1,-1],
-              [-1,-1,-1,-1,-1,-1,-1]]
+# Test unitaire de la fonction coup_gagnant
+def test_coup_gagnant():
+    grille = [
+        [1,  1,  1, -1, -1, -1, -1],
+        [0,  0,  0, -1, -1, -1, -1],
+        [0,  0,  0, -1, -1, -1, -1],
+        [0,  0,  0,  0,  0, -1, -1],
+        [0,  0,  0,  0,  0, -1, -1],
+        [0,  0,  0,  0,  0,  0, -1]
+    ]
+    assert coup_gagnant(1, 0, 0, grille) == True
+    assert coup_gagnant(0, 0, 0, grille) == False
 
-    niveau = [6,6,6,6,6,6,6]
+# Test unitaire de la fonction changer_joueur
+def test_changer_joueur():
+    assert changer_joueur(0) == 1
+    assert changer_joueur(1) == 0
 
-    # joueur 1 joue la case 00
-    joueur = 1
-    case = 'b0'
-    maj_jeu(case, grille, niveau, joueur)
-    assert grille == [[-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [1 ,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1]]
-    assert niveau == [5,6,6,6,6,6,6]
-
-    # joueur 0 joue la case 01
-    joueur = 0
-    case = 'b1'
-    maj_jeu(case, grille, niveau, joueur)
-    assert grille == [[-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [1 ,0 ,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1]]
-    assert niveau == [5,5,6,6,6,6,6]
-
-    # joueur 1 joue la case 11
-    joueur = 1
-    case = 'b1'
-    maj_jeu(case, grille, niveau, joueur)
-    assert grille == [[-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1],
-                      [1 ,0 ,1 ,-1,-1,-1,-1],
-                      [-1,-1,-1,-1,-1,-1,-1]]
-    assert niveau == [5,4,6,6,6,6,6]
+# Test d'intégration du programme principal
+def test_programme_principal():
+    with patch.object(sys, 'argv', ['main.py']):
+        construire_gui()
+        # Tester l'interface graphique est difficile sans fenêtre graphique
+        # On ne peut pas faire de test de bouton sans simulation de clics
